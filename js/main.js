a3f47e9dc8fe0ad8e4e1e390100698d5b56f287d
Zahari Dimitrov <z.dimitrov98@gmail.com>
2020-01-11 21:15:59 +0100

    dropdown

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
-//function openNav() {
-//  document.getElementById("mySidenav").style.width = "250px";
-//  document.getElementById("main").style.marginLeft = "250px";
-//}
-//
-//
-//function closeNav() {
-//  document.getElementById("mySidenav").style.width = "0";
-//  document.getElementById("main").style.marginLeft = "0";
-//}
-
 function openNav() {
   document.getElementById("mySidenav").style.height = "100%";
+  document.getElementById("main").style.marginTop = "100%";
 }
 
-/* Close/hide the sidenav */
+
 function closeNav() {
   document.getElementById("mySidenav").style.height = "0";
+  document.getElementById("main").style.marginTop = "0";
 }
+
+//function openNav() {
+//  document.getElementById("mySidenav").style.height = "100%";
+//}
+//
+///* Close/hide the sidenav */
+//function closeNav() {
+//  document.getElementById("mySidenav").style.height = "0";
+//}
 
 $(window).load(function() {
 		// Animate loader off screen
