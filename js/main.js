34d37af639c8a3089a028d6ff097967295922c6d
Zahari Dimitrov <z.dimitrov98@gmail.com>
2020-01-12 12:49:51 +0100

    Footnotes, progress bar

--- js/main.js
+++ js/main.js
@@ -1,24 +1,23 @@
 function openNav() {
   document.getElementById("mySidenav").style.height = "100%";
-  document.getElementById("main").style.marginTop = "100%";
 }
 
-
+/* Close/hide the sidenav */
 function closeNav() {
   document.getElementById("mySidenav").style.height = "0";
-  document.getElementById("main").style.marginTop = "0";
 }
-
-//function openNav() {
-//  document.getElementById("mySidenav").style.height = "100%";
-//}
-//
-///* Close/hide the sidenav */
-//function closeNav() {
-//  document.getElementById("mySidenav").style.height = "0";
-//}
 
 $(window).load(function() {
 		// Animate loader off screen
 		$(".loader").delay(10000).fadeTo("slow", 0);;
 	});
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
