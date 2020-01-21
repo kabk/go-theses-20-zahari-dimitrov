57ba80b51eb4b6615bacb347b34102f536723e70
Zahari Dimitrov <z.dimitrov98@gmail.com>
2020-01-22 00:10:57 +0100

    Collectives *

--- js/main.js
+++ js/main.js
@@ -125,5 +125,5 @@ $(document).ready(function () {
     // Handler for .ready() called.
     $('html, body').animate({
         scrollTop: $('.title').offset().top
-    }, 5000);
+    }, 9000);
 });
